74cad3b86640e1614d2d5a6e7f65b28151fed581
AlAnoud <Alanoudalbattah@outlook.com>
2021-09-07 23:15:34 +0300

    fix: Deprecation in the changing of the dialect

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+#SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+#! replaced due to deprecation in the changing of the dialect 
+#src: https://stackoverflow.com/questions/66690321/flask-and-heroku-sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 EXPLAIN_TEMPLATE_LOADING = True
